872ec774764ec7c71ba531baaf5a98be8f43d7ca
David Gonzalez Solana <dgonzalezsolana@gmail.com>
2020-12-04 13:06:34 +0100

    Add swagger

--- src/main/kotlin/me/deblugger/Application.kt
+++ src/main/kotlin/me/deblugger/Application.kt
@@ -1,10 +1,24 @@
 package me.deblugger
 
 import io.micronaut.runtime.Micronaut.*
-fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("me.deblugger")
-		.start()
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+
+@OpenAPIDefinition(
+	info = Info(
+			title = "Yira",
+			version = "0.1",
+			description = "Fake yira project"
+	)
+)
+// Work around
+object Application {
+	@JvmStatic
+	fun main(args: Array<String>) {
+		build()
+				.args(*args)
+				.packages("me.deblugger")
+				.start()
+	}
 }
 
